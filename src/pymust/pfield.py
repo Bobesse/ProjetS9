f0934887f39af61d6023985218708918b744bda6
DESKTOP-TIBO\tlali <tl.alias.forums@gmail.com>
2024-02-18 17:50:09 +0100

    Working example

--- src/pymust/pfield.py
+++ src/pymust/pfield.py
@@ -1,6 +1,8 @@
 
 import numpy as np
 from . import utils
+import tqdm
+
 
 # Ugly optimisation trick, of loop unraveling, as np.mean/np.sum has a large overhead for iterating over few dimesions
 # for i in range(1, 10):
@@ -723,7 +725,7 @@ def pfield(x : np.ndarray,y : np.ndarray, z: np.ndarray, delaysTX : np.ndarray, 
         SPECT = np.zeros((nSampling,nx),dtype=np.complex64)
     EXP = EXP.astype(np.complex64)
     # TODO GB: process several frequencies at the same time might remove some overhead of numpy calls
-    for k  in range(nSampling):
+    for k  in tqdm.tqdm(range(nSampling)):
 
         kw = 2*np.pi*f[k]/c #; % wavenumber
         
@@ -949,3 +951,114 @@ def MGBMcoeff(n):
         raise ValueError()
     
     return A, B
+
+
+
+
+MoyenneSousElemPiezo = average_over_last_axis
+
+def mypfield(x : np.ndarray,y : np.ndarray,z : np.ndarray,DelaiEmissionElemPiezo  : np.ndarray,param):
+    FrequencyStep = 1 # coef de diffusion
+    RP = 0 # RP = Radiation Pattern
+    # RP = np.zero((-1,param.Nelements, delaysTX.shape[0] )) 
+    RadiusOfCurvature = param.radius
+    c = 1540 # % default value of sound speed
+    OriginalShape = x.shape
+
+    # FREQUENCY SAMPLES
+    # param.fc = central frequency (Hz)
+    Nf = int( 2* np.ceil( param.fc )+1) # number of frequency samples
+    Nf = 10000
+    f = np.linspace(0,2*param.fc,Nf) # frequency samples
+    nSampling = len(f)
+
+    # Position des elements de la sonde
+    # THe = angle of the normal to element with respect to the z-axis
+    ElementWidth = param.width
+    NumberOfElements = param.Nelements
+    PositionsCentresElemPiezo_x, PositionsCentresElemPiezo_z, AngleNormalElemPiezo, _ = param.getElementPositions()
+    # AngleNormalElemPiezo = 0
+    PositionsCentresElemPiezo_x = PositionsCentresElemPiezo_x.reshape((1,NumberOfElements,1))
+    PositionsCentresElemPiezo_z = PositionsCentresElemPiezo_z.reshape((1,NumberOfElements,1))
+
+    ResolutionSpatiale = c/(param.fc*(1+param.bandwidth/200))
+
+    NbSousElemPiezo = int(np.ceil(ElementWidth/ResolutionSpatiale))
+    #NbSousElemPiezo=1
+    SizeSousElemPiezo = ElementWidth/NbSousElemPiezo
+
+    PositionsCentresSousElemPiezo = -ElementWidth/2 + SizeSousElemPiezo/2 + np.arange(NbSousElemPiezo)*SizeSousElemPiezo
+    PositionsCentresSousElemPiezo = PositionsCentresSousElemPiezo.reshape((1,1,NbSousElemPiezo)) # une valeur x,z pour chaque SousElemPiezo
+
+    PositionsCentresSousElemPiezo_x = PositionsCentresSousElemPiezo*np.cos(AngleNormalElemPiezo)[:,:,np.newaxis]
+    PositionsCentresSousElemPiezo_z = PositionsCentresSousElemPiezo*np.sin(-AngleNormalElemPiezo)[:,:,np.newaxis]
+
+   
+    #%-- Coordinates of the points where pressure is needed
+    x = x.reshape((-1,1,1)).astype(np.float32)
+    y = y.reshape((-1,1,1)).astype(np.float32)
+    z = z.reshape((-1,1,1)).astype(np.float32)
+
+
+    FreqSweep = param.TXfreqsweep
+    #%-- FREQUENCY SPECTRUM of the transmitted pulse
+    pulseSpectrum = param.getPulseSpectrumFunction(FreqSweep)
+    #%-- FREQUENCY RESPONSE of the ensemble PZT + probe
+    probeSpectrum = param.getProbeFunction()
+    pulseSPECT = pulseSpectrum(2*np.pi*f) # pulse spectrum
+    probeSPECT = probeSpectrum(2*np.pi*f) # probe 
+
+  
+    # valeurs de RP pour chaque point de la grille pour chaque sous element piezo de chaque elem de la sonde 
+    dxi = x.reshape((-1,1,1)) - PositionsCentresSousElemPiezo_x - PositionsCentresElemPiezo_x
+    dzi = z.reshape((-1,1,1)) - PositionsCentresSousElemPiezo_z - PositionsCentresElemPiezo_z
+    dyi = y.reshape((-1,1,1))
+    r2 = dxi**2 + dzi**2 + dyi**2 # pythagore 3d
+
+    # Pas de limite inferieure de resolution spatiale
+    # Tout peut etre aussi petit que cenessaire, pas de probleme
+    # smallD2 = ResolutionSpatiale**2
+    # d2[d2<smallD2] = smallD2
+
+    r = np.sqrt(r2).astype(np.float32) # distance between the segment centroid and the point of interest
+    # On considere que la frequemce est independente de la direction du signal emis/recu. Ne depend uniquement de la distance (r)
+    # Th = np.arcsin(dxi/r)-AngleNormalElemPiezo.reshape((1,-1,1))
+    del r2
+
+    # # %-- EXPONENTIAL arrays of size [numel(x) NumberOfElements M]
+    wavenumber = 2*np.pi*f[0]/c # % wavenumber
+    kwa = 0 # % attenuation-based wavenumber; no attenuation -> 0
+    # EXP = np.exp(-kwa*r + 1j*np.mod(kw*r,2*np.pi)).astype(np.complex64) #; % faster than exp(-kwa*r+1j*kw*r)
+    EXP = np.exp(-kwa*r+1j*wavenumber*r).astype(np.complex64)
+
+    dkw = 2*np.pi*FrequencyStep/c
+    dkwa = 0
+    delta_EXP = np.exp(-dkwa*r + 1j*dkw*r).astype(np.complex64)
+
+    del r
+    
+    for k  in tqdm.tqdm(range(nSampling)):
+
+        wavenumber_k = 2*np.pi*f[k]/c #; % wavenumber
+
+        EXP = EXP*delta_EXP
+
+        ValeurElemPiezo = MoyenneSousElemPiezo(EXP); #% summation over the M small segments
+        
+        DelaiEmissionElemPiezo = DelaiEmissionElemPiezo.astype(np.float32)
+
+        APOD = 1 # pas d'apodization -> APOD=1
+        DELAPOD = APOD * np.sum(np.exp(1j*wavenumber_k*c*DelaiEmissionElemPiezo), 0).reshape((-1, 1)) # force (NumberofElements,1)
+
+        RPk = np.matmul(ValeurElemPiezo, DELAPOD) #somme des radiation pattern de chaque élément
+        
+        RPk = pulseSPECT[k]*probeSPECT[k]*RPk #%- include spectrum responses:
+
+        RP += np.abs(RPk)**2
+
+
+    RP = np.sqrt(RP); #% acoustic intensity, RPk est le radiation pattern de chaque élément
+
+    CorrectingFactor = FrequencyStep
+    RP = RP*CorrectingFactor
+    return RP.reshape(OriginalShape)
